f3bc2bae3197513d784820e846dc88d32440a431
Math3Faria <matheusteud12@gmail.com>
2026-09-18 11:19:10 -0300

    corrigindo erros do app.tsx

--- App.tsx
+++ App.tsx
@@ -1,42 +1,53 @@
+import React from "react"
+import {Text} from "react-native"
 import {NavigationContainer} from "@react-navigation/native"
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs"
-import {Text} from "react-native"
 
+import HomeScreen from "./screens/HomeScreen"
 import ExploreScreen from "./screens/ExploreScreen"
 import FavoritesScreen from "./screens/FavoriteScreen"
-import React from "react"
 
-const Tab = createBottomTabNavigator()
+const Tab=createBottomTabNavigator()
 
-export default function App() {
-  return (
+export default function App(){
+  return(
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={{
-          headerShown: false,
-          tabBarStyle: {
-            backgroundColor: "#11152A",
-            borderTopColor: "#252A42",
-            height: 70,
-            paddingTop: 8,
-            paddingBottom: 8
+          headerShown:false,
+          tabBarStyle:{
+            backgroundColor:"#11152A",
+            borderTopColor:"#252A42",
+            height:70,
+            paddingTop:8,
+            paddingBottom:8
           },
-          tabBarActiveTintColor: "#FFFFFF",
-          tabBarInactiveTintColor: "#747B92",
-          tabBarLabelStyle: {
-            fontSize: 12,
-            fontWeight: "600"
+          tabBarActiveTintColor:"#FFFFFF",
+          tabBarInactiveTintColor:"#747B92",
+          tabBarLabelStyle:{
+            fontSize:12,
+            fontWeight:"600"
           }
         }}
       >
-        
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel:"Home",
+            tabBarIcon:()=>(
+              <Text style={{fontSize:22}}>🌌</Text>
+            )
+          }}
+        />
+
         <Tab.Screen
           name="Explore"
           component={ExploreScreen}
           options={{
-            tabBarLabel: "Explorar",
-            tabBarIcon: ({color}) => (
-              <Text style={{fontSize: 22, color}}>🔭</Text>
+            tabBarLabel:"Explorar",
+            tabBarIcon:()=>(
+              <Text style={{fontSize:22}}>🔭</Text>
             )
           }}
         />
@@ -45,13 +56,12 @@ export default function App() {
           name="Favorites"
           component={FavoritesScreen}
           options={{
-            tabBarLabel: "Favoritos",
-            tabBarIcon: ({color}) => (
-              <Text style={{fontSize: 22, color}}>♥</Text>
+            tabBarLabel:"Favoritos",
+            tabBarIcon:({color})=>(
+              <Text style={{fontSize:25,color}}>♥</Text>
             )
           }}
         />
-
       </Tab.Navigator>
     </NavigationContainer>
   )
